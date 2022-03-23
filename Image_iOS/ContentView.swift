28fbc6492a107955467e0a01c19b0b716d148a34
subrota <subrotadebnath1807@gmail.com>
2022-03-24 01:18:15 +0600

    Initial Commit

---
+++ Image_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Image_iOS
+//
+//  Created by Subrota Debnath on 24/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Image_iOS/Image_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_iOSApp.swift
+//  Image_iOS
+//
+//  Created by Subrota Debnath on 24/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Image_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
